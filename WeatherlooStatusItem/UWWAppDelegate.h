f45e9a4261d670cfc70b5a07a06afad6485bb747
Connor Cimowsky <connorcimowsky@me.com>
2014-01-14 19:08:52 -0500

    Initial commit.

---
+++ WeatherlooStatusItem/UWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UWWAppDelegate.h
+//  WeatherlooStatusItem
+//
+//  Created by Connor Cimowsky on 1/14/14.
+//  Copyright (c) 2014 Connor Cimowsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UWWAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ WeatherlooStatusItem/UWWAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  UWWAppDelegate.m
+//  WeatherlooStatusItem
+//
+//  Created by Connor Cimowsky on 1/14/14.
+//  Copyright (c) 2014 Connor Cimowsky. All rights reserved.
+//
+
+#import "UWWAppDelegate.h"
+
+@implementation UWWAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
